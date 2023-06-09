3a3ed220aed0e835265cbc4f40a3fd57196b66bb
Prince Codes <princecodes247@gmail.com>
2023-06-09 10:27:15 +0100

    update final

--- index.js
+++ index.js
@@ -1,30 +1,82 @@
 const express = require("express");
-const axios = require("axios");
+var unirest = require("unirest");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3005;
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
 
-app.all("/*", async (req, res) => {
+// Disable SSL certificate verification
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+app.post("/subscribe", async (req, res) => {
   try {
-    const targetUrl = "https://mailer.punteer.com/" + req.url;
-    const response = await axios({
-      method: req.method,
-      url: targetUrl,
-      headers: req.headers,
-      data: req.body,
-    });
+    // const targetUrl = "http://mailer.punteer.com" + req.url;
+    const targetUrl = "https://mailer.punteer.com/sender/subscribe";
+    if (!req.body.email) {
+      return res.status(400).json({ message: "Email is required" });
+    }
+
+    var req = unirest("POST", targetUrl)
+      .headers({
+        "Content-Type": "application/x-www-form-urlencoded",
+      })
+      .send(`api_key=${process.env.API_KEY}`)
+      .send(`list=${process.env.LIST_ID}`)
+      .send("email=" + req.body.email)
+      .send(`name=${req?.body?.name ?? ""}`)
+      .send("boolean=true")
+      .end(function (resp) {
+        if (resp.error) throw new Error(resp.error);
+        console.log({ resp });
+        res.status(resp.status).json({ data: resp.body });
+      });
+
+    // console.log({ response: response.body });
+    // console.log({ responseData });
 
     // Forward the response headers
-    Object.keys(response.headers).forEach((key) => {
-      res.setHeader(key, response.headers[key]);
-    });
-
-    res.status(response.status).send(response.data);
+    // response.headers.forEach((value, key) => {
+    //   res.setHeader(key, value);
+    // });    res.json({ message: "success" });
+    // res.status(response.status).json(responseData);
   } catch (error) {
+    console.log({ error });
+    res.status(500).send("Error proxying the request");
+  }
+});
+
+app.get("/count", async (req, res) => {
+  try {
+    // const targetUrl = "http://mailer.punteer.com" + req.url;
+    const targetUrl =
+      "https://mailer.punteer.com/sender/api/subscribers/active-subscriber-count.php";
+
+    var req = unirest("POST", targetUrl)
+      .headers({
+        "Content-Type": "application/x-www-form-urlencoded",
+      })
+      .send(`api_key=${process.env.API_KEY}`)
+      .send(`list_id=${process.env.LIST_ID}`)
+      .send("boolean=true")
+      .end(function (resp) {
+        if (resp.error) throw new Error(resp.error);
+        console.log({ resp });
+        res.status(resp.status).json({ data: resp.body });
+      });
+
+    // console.log({ response: response.body });
+    // console.log({ responseData });
+
+    // Forward the response headers
+    // response.headers.forEach((value, key) => {
+    //   res.setHeader(key, value);
+    // });
+    // res.status(response.status).json(responseData);
+  } catch (error) {
+    console.log({ error });
     res.status(500).send("Error proxying the request");
   }
 });
